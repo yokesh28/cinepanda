80407a8235e34791b9f02a40630660c1253a7425
lokesh <yokesh.ganesan@gmail.com>
2015-08-18 23:25:22 +0530

    remote database

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cinepanda');
+define('DB_NAME', 'smarzrxr_cinepanda');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'smarzrxr_panda');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'smartersworld');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
